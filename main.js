49cd04ed7d7b9033bf81dabad91fd9c673bd7e8a
yoonjong.park <yoonjongpark@gmail.com>
2020-10-04 23:31:17 +0900

    main.js : ADD filtering project
    work project filtering.

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 'use strict';  // strict Mode // ES5부터 추가됨. IE10부터 지원.  https://bit.ly/3kIfpms
 
-
 // Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');  // 왜 getElementById를 안쓰고 querySelector를 쓰는 걸까? 정확히 모르겠다.
 const navbarHeight = navbar.getBoundingClientRect().height;
@@ -28,10 +27,9 @@ navbarMenu.addEventListener('click', function(event) {
     const scrollMove = document.querySelector(link);
     // scrollMove.scrollIntoView({behavior: "smooth"});
     const top = scrollMove.offsetTop - navbarHeight < 0 ? 0 : scrollMove.offsetTop - navbarHeight;  // top - navbarHeight < 0 일 때 0으로 입력.
-    const left = scrollMove.offsetLeft;
     window.scrollTo({
         top:top,
-        left:left,
+        left:0,
         behavior: 'smooth'
     });
 });
@@ -43,12 +41,15 @@ homeContactBtn.addEventListener('click', () => {
     scrollIntoView('#contact');
 });
 
+
+// Home opacity
 const homeContainer = document.querySelector('.home__container');
 const homeContainerHeight = homeContainer.getBoundingClientRect().height;
 
 document.addEventListener('scroll', function(event) {
     homeContainer.style.opacity = 1.1 - window.scrollY / homeContainerHeight;
 });
+
 
 // Show "arrow up" Button when scrolling down
 const arrowUp = document.querySelector('.arrow-up');
@@ -63,6 +64,25 @@ arrowUp.addEventListener('click',function(event){
     scrollIntoView('#home');
 });
 
+//Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', function(event) {
+    const filter = event.target.dataset.filter || event.target.parentNode.dataset.filter;
+    // console.log(filter);
+    if(filter == null) {  // 빈 곳 클릭 시 넘기기.
+        return;
+    }
+    projects.forEach((project) => {
+        console.log(project.dataset.type);
+        if(filter === '*' || filter === project.dataset.type) {
+            project.classList.remove('invisible');
+        } else {
+            project.classList.add('invisible');
+        }
+    });
+});
 
 
 
